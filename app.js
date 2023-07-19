0ac2f05801d5a0c12742c72b1329d7edeba9f9e0
shahzadgamedev <personalshahzadansari@gmail.com>
2023-07-19 21:09:12 +0530

    Added Switch Statement To Dispaly Day of Week

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
@@ -11,28 +11,44 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
 
 
-var today = new Date();
-var currentDay = today.getDay();
-var day = ""; currentDay = 0;
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
 
-if (currentDay === 6 || currentDay === 0) {
-    res.render('list', {KindofDay: 'WeekEnd'});
-} else {
-    res.render('list', {KindofDay: 'WeekDay'});
-}
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error: current day is equal to: " + currentDay);
+            break;
+    }
 
-    
-
-
-
-
-
+    res.render('list', { KindofDay: day });
 
 });
 
 
 
 // listen To Port 80
-app.listen('80','192.168.29.131', () => {
+app.listen('80', '192.168.29.131', () => {
     console.log('server started');
 });
